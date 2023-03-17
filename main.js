949c67f92cbc32d21291fe12776563b412607eed
evadou14 <105525126+evadou14@users.noreply.github.com>
2023-03-16 22:39:19 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload(){
+
+}
+function setup(){
+    var canvas = createCanvas(340,340);
+    canvas.center();
+    canvas.background("white");
+}
+function draw(){
+
+}
+function saveImage(){
+    
+}
